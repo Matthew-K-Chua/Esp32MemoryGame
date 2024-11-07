52ccd0f69620b75ab01246cbf05d7312d80816bf
AceOf5pade5 <creativenerf@gmail.com>
2024-11-08 06:34:36 +1100

    Wrote unit tests
    Built, but nothing is tested yet because I don't have my hardware with me.

--- src/main.cpp
+++ src/main.cpp
@@ -94,44 +94,6 @@ int displaySequence(int sequence) {
   }
 }
 
-void mattsCookedArrayIndexReturnTest() {
-  Serial.println("/////////////////////////////////");
-  Serial.println("TESTING mattsCookedArrayIndexReturn");
-  int testArray = 123456789;
-  int indexOne = 1;
-  int indexTwo = 2;
-  int indexNine = 9;
-  int totalPass = 0;
-  int valueAtIndexOne = mattsCookedArrayIndexReturn(testArray, indexOne);
-  int valueAtIndexTwo = mattsCookedArrayIndexReturn(testArray, indexTwo);
-  int valueAtIndexNine = mattsCookedArrayIndexReturn(testArray, indexNine);
-  if (valueAtIndexOne == indexOne) {
-    Serial.println("indexOne passed");
-    totalPass++;
-  } else {
-    Serial.println("indexOne failed");
-  }
-
-  if (valueAtIndexTwo == indexTwo) {
-    Serial.println("indexTwo passed");
-    totalPass++;
-  } else {
-    Serial.println("indexTwo failed");
-  }
-
-  if (valueAtIndexNine == indexNine) {
-    Serial.println("indexNine passed");
-    totalPass++;
-  } else {
-    Serial.println("indexNine failed");
-  }
-
-  if (totalPass == 3) {
-    Serial.println("mattsCookedArrayIndexReturn passed");
-  }
-
-}
-
 int getPlayerInput() { // verify that this outputs the correct things, otherwise change the numbers
   int input = 0;
   while (!waitForInput()) {
@@ -173,19 +135,7 @@ bool playerTurn(int sequence) {
     }
   }
   flashLights();
-}
-
-
-void testHardware() {
-  // test LEDS on and off
-  // test the joystick
-  // test the joystick button
-}
-
-void waitForInputTest() {
-  // add a printout saying start, don't move
-  // add a printout saying move now
-  // add a printout after input is received
+  return roundWon;
 }
 
 void playGame() {
@@ -199,4 +149,143 @@ void playGame() {
     roundWon = playerTurn(sequence);
 }
 
+// TESTS //
 
+void testHardware() {
+  // test LEDS on and off
+  // test the joystick
+  // test the joystick button
+}
+
+void waitForInputTest() {
+  // add a printout saying start, don't move
+  // add a printout saying move now
+  // add a printout after input is received
+}
+
+void showLightTest() {
+  Serial.println("/////////////////////////////////");
+  Serial.println("TESTING showLight");
+  int ledValue = HIGH;
+  digitalWrite(yellowLED, ledValue);
+  Serial.println("Yellow On");
+  digitalWrite(redLED, ledValue);
+  Serial.println("Red On");
+  digitalWrite(blueLED, ledValue);
+  Serial.println("Blue On");
+  digitalWrite(greenLED, ledValue);
+  Serial.println("Green On");
+  delay(500);
+  ledValue = LOW;
+  digitalWrite(yellowLED, ledValue);
+  digitalWrite(redLED, ledValue);
+  digitalWrite(blueLED, ledValue);
+  digitalWrite(greenLED, ledValue);
+  Serial.println("Test Complete");
+}
+
+void flashLightsTest() {
+  Serial.println("/////////////////////////////////");
+  Serial.println("TESTING flashLights");
+  flashLights();
+  Serial.println("Test Complete");
+}
+
+
+void mattsCookedArrayAppendTest() {
+  Serial.println("/////////////////////////////////");
+  Serial.println("TESTING mattsCookedArrayAppend");
+  int array = 1234;
+  int nextValue = 5;
+  int arrayLength = floor(log(array)/log(10)); // get the length of the array
+  array += pow(10,arrayLength+1)*nextValue;
+  if (array == 12345) {
+    Serial.println("mattsCookedArrayAppend passed");
+  } else {
+    Serial.println("mattsCookedArrayAppend failed");
+  }
+  Serial.println("Test Complete");
+}
+
+void createSequenceTest() {
+  Serial.println("/////////////////////////////////");
+  Serial.println("TESTING createSequence");
+  int sequence = 1234;
+  int nextInt = random(4); // get a random 1/2/3/4
+  if (sequence != NULL) {
+    sequence = mattsCookedArrayAppend(sequence, nextInt); // "append next in sequence"
+  } else {
+    sequence = nextInt; // if the game has only begun, set sequence to the first rand int
+  }
+  if (sequence >= 12341 || sequence <= 12344) {
+    Serial.println("createSequence passed");
+  }
+  Serial.println("Test Complete");
+}
+
+void mattsCookedArrayIndexReturnTest() {
+  Serial.println("/////////////////////////////////");
+  Serial.println("TESTING mattsCookedArrayIndexReturn");
+  int testArray = 123456789;
+  int indexOne = 1;
+  int indexTwo = 2;
+  int indexNine = 9;
+  int totalPass = 0;
+  int valueAtIndexOne = mattsCookedArrayIndexReturn(testArray, indexOne);
+  int valueAtIndexTwo = mattsCookedArrayIndexReturn(testArray, indexTwo);
+  int valueAtIndexNine = mattsCookedArrayIndexReturn(testArray, indexNine);
+  if (valueAtIndexOne == indexOne) {
+    Serial.println("indexOne passed");
+    totalPass++;
+  } else {
+    Serial.println("indexOne failed");
+  }
+
+  if (valueAtIndexTwo == indexTwo) {
+    Serial.println("indexTwo passed");
+    totalPass++;
+  } else {
+    Serial.println("indexTwo failed");
+  }
+
+  if (valueAtIndexNine == indexNine) {
+    Serial.println("indexNine passed");
+    totalPass++;
+  } else {
+    Serial.println("indexNine failed");
+  }
+
+  if (totalPass == 3) {
+    Serial.println("mattsCookedArrayIndexReturn passed");
+  }
+  Serial.println("Test Complete");
+}
+
+void displaySequenceTest() {
+  Serial.println("/////////////////////////////////");
+  Serial.println("TESTING displaySequence");
+  int sequence = 1234;
+  int next;
+  int sequenceLength = floor(log(sequence)); // get the power of 10 that the sequence is up to
+  for (int i=1;sequenceLength;i++) {
+    next = mattsCookedArrayIndexReturn(sequence, i);
+    showLight(next, HIGH);
+    delay(500);
+    showLight(next, LOW);
+  }
+  Serial.println("Test Complete");
+}
+
+void getPlayerInputTest() { // each input will illuminate the correct bulb for 5 seconds
+  Serial.println("/////////////////////////////////");
+  Serial.println("TESTING getPlayerInput");
+  int currentTime = millis();
+  int stopTime = currentTime + 5000;
+  while (currentTime < stopTime) {
+    int input = getPlayerInput();
+    showLight(input, HIGH);
+    delay(100);
+    showLight(input, LOW);
+  }
+  Serial.println("Test Complete");
+}
