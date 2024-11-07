82d7efc30d31fd0d805d649b273e94f80db2c295
AceOf5pade5 <creativenerf@gmail.com>
2024-11-07 18:48:31 +1100

    Array attempt
    Because C is so low level, it's a trek to use arrays. I could use an array, but I'd rather not. I will use a number like an array.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,45 @@
 #include <Arduino.h>
+int * createSequence(int sequence[]) {
+
+  return sequence;
+}
+int displaySequence(int sequence) {
+  
+}
+void flashLights() {
+
+}
+bool playerTurn(int sequence) {
+
+}
+
 
 void setup() {
   Serial.begin(9600);
+  randomSeed(analogRead(0));
 }
 
 void loop() {
 }
+
+void waitForInput() {
+  int inputFlag = 0;
+  while (inputFlag == 0) {}
+  return;
+}
+
+void playGame() {
+  int sequence = {};
+  bool roundWon = true;
+  while (roundWon == true)
+    int nextInt = random(4);
+    int sequenceLength = sizeof(sequence);
+    sequence[sequenceLength] = nextInt;
+    displaySequence(sequence);
+    flashLights();
+  bool roundWon = playerTurn(sequence);
+  if (roundWon == true) {
+    flashLights();
+  }
+}
+
